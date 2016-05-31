1dd505fe5626d8c0afcb788d46ba61ffbbf4a4d1
carlosmartinez <cdmart12@asu.edu>
2016-05-31 11:57:12 -0700

    Added JavaDoc comments

--- cse360assign1/src/cse360assign2/Analytics.java
+++ cse360assign1/src/cse360assign2/Analytics.java
@@ -6,6 +6,12 @@ public class Analytics extends OrderedIntList{
 	public int high = 0;
 	public int low = 0;
 	
+	/**
+	   * This method is used to add each element in the array's values and divide
+	   * them by the number of elements in the array
+	   * @param a user-entered array
+	   * @return the evaluated mean
+	   */
 	public double mean(Analytics value){
 		if(counter != 0){
 			mean = 0;
@@ -19,7 +25,11 @@ public class Analytics extends OrderedIntList{
 		}
 		return mean;
 	}
-	
+	/**
+	   * This method is used to print the median of the array
+	   * @param a user-entered array
+	   * @return the evaluated median
+	   */
 	public int median(Analytics value){
 		if(counter != 0){
 			if(counter % 2 == 0){
@@ -36,6 +46,12 @@ public class Analytics extends OrderedIntList{
 		return median;
 	}
 	
+	/**
+	   * This method is used to print the last value in the array which is also
+	   * the largest value in the array
+	   * @param a user-entered array
+	   * @return the highest value
+	   */
 	public int high(Analytics value){
 		if(counter != 0){
 			high = value.array[counter-1];
@@ -46,6 +62,12 @@ public class Analytics extends OrderedIntList{
 		return high;
 	}
 	
+	/**
+	   * This method is used to print the first value in the array which is also
+	   * the smalles value in the array
+	   * @param a user-entered array
+	   * @return the lowest value
+	   */
 	public int low(Analytics value){
 		if(counter != 0){
 			low = value.array[0];
@@ -55,7 +77,11 @@ public class Analytics extends OrderedIntList{
 		}
 		return low;
 	}
-	
+	/**
+	   * This method is used to return the number of values in the array
+	   * @param a user-entered array
+	   * @return the number of values
+	   */
 	public int numInts(Analytics value){
 		return counter;
 	}
